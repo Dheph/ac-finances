28c838633b3f83bd6eacc507a438c6c5fd525b8e
Dhepherson Ribeiro <dhephersonribeiro@Dhephersons-MacBook-Air.local>
2023-07-04 20:10:12 -0300

    add: initial structure

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,38 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "REACT_APP_API_BASE_URL",
+          moduleName: "@env",
+          path: ".env",
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          alias: {
+            "@assets": "./src/assets",
+            "@pages": "./src/pages",
+            "@components": "./src/components",
+            "@services": "./src/services",
+            "@errors": "./src/errors",
+            "@theme": "./src/theme",
+          },
+        },
+      ],
+    ],
   };
 };
---
+++ src/theme/index.ts
@@ -0,0 +1,21 @@
+export default {
+  colors: {
+    //default colors
+    primary_800: "#12A454",
+    primary_400: "#21E79F",
+    primary_200: "#33CC95",
+
+    //common colors
+    background: "#F0F2F5",
+    text: "#969CB2",
+    opacity_text: "#969CB2",
+    title_text: "#363F5F",
+    debit: "#E52E4D",
+  },
+  //basic fonts
+  fonts: {
+    regular: "Roboto_400Regular",
+    medium: "Roboto_500Medium",
+    bold: "Roboto_700Bold",
+  },
+};
