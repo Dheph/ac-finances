c6a04ae23be8792a7efa8718f7bdcc1ce4fa9c18
Dhepherson Ribeiro <dhephersonribeiro@Dhephersons-MacBook-Air.local>
2023-07-07 12:26:41 -0300

    add: check availables updates,fonts, theme provider and home screen call

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import 'react-native-reanimated';
+import 'react-native-gesture-handler';
+
+import React, { useEffect } from 'react';
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import theme from '@theme/index';
+import AppLoading from 'expo-app-loading';
+import * as Updates from 'expo-updates';
+
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { Home } from '@pages/Home';
 
 export default function App() {
+  useEffect(() => {
+    async function checkAvailableUpdatesAndUpdate() {
+      const { isAvailable } = await Updates.checkForUpdateAsync();
+
+      if (isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    }
+    checkAvailableUpdatesAndUpdate();
+  }, []);
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>AC Finances </Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <Home />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
