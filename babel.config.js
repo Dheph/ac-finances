01087bd0dfbd65dcc4cf573d0cb2f6eab56ee1e9
Dhepherson Ribeiro <dhephersonribeiro@Dhephersons-MacBook-Air.local>
2023-07-07 12:17:06 -0300

    update: babel config plugins and alias

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,15 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
     plugins: [
-      "react-native-reanimated/plugin",
+      'react-native-reanimated/plugin',
       [
-        "module:react-native-dotenv",
+        'module:react-native-dotenv',
         {
-          envName: "REACT_APP_API_BASE_URL",
-          moduleName: "@env",
-          path: ".env",
+          envName: 'REACT_APP_API_BASE_URL',
+          moduleName: '@env',
+          path: '.env',
           blocklist: null,
           allowlist: null,
           safe: false,
@@ -17,19 +17,21 @@ module.exports = function (api) {
           verbose: false,
         },
       ],
-      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
       [
-        "module-resolver",
+        'module-resolver',
         {
-          root: ["./src"],
-          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
           alias: {
-            "@assets": "./src/assets",
-            "@pages": "./src/pages",
-            "@components": "./src/components",
-            "@services": "./src/services",
-            "@errors": "./src/errors",
-            "@theme": "./src/theme",
+            '@assets': './src/assets',
+            '@pages': './src/pages',
+            '@components': './src/components',
+            '@context': './src/context',
+            '@services': './src/services',
+            '@errors': './src/errors',
+            '@theme': './src/theme',
+            '@utils': './src/utils',
           },
         },
       ],
