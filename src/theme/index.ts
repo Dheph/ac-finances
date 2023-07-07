ff931027cb023c57c4795c7c442e800338b87384
Dhepherson Ribeiro <dhephersonribeiro@Dhephersons-MacBook-Air.local>
2023-07-07 12:28:28 -0300

    add: theme colors and fonts

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,21 +1,24 @@
 export default {
   colors: {
     //default colors
-    primary_800: "#12A454",
-    primary_400: "#21E79F",
-    primary_200: "#33CC95",
+    primary_800: '#12A454',
+    primary_400: '#21E79F',
+    primary_200: '#33CC95',
 
     //common colors
-    background: "#F0F2F5",
-    text: "#969CB2",
-    opacity_text: "#969CB2",
-    title_text: "#363F5F",
-    debit: "#E52E4D",
+    background: '#F0F2F5',
+    text: '#969CB2',
+    opacity_text: 'rgba(150, 156, 178, 1)',
+    title_text: '#363F5F',
+    debit: '#E52E4D',
+    white: '#FFFFFF',
+    black: '#000000',
+    input_background: 'rgba(215, 215, 215, 1)',
   },
   //basic fonts
   fonts: {
-    regular: "Roboto_400Regular",
-    medium: "Roboto_500Medium",
-    bold: "Roboto_700Bold",
+    regular: 'Roboto_400Regular',
+    medium: 'Roboto_500Medium',
+    bold: 'Roboto_700Bold',
   },
 };
