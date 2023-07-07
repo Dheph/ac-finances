d12daea631b1fd969ec734a068e1e007d40f9ae2
Dhepherson Ribeiro <dhephersonribeiro@Dhephersons-MacBook-Air.local>
2023-07-07 12:31:37 -0300

    add: home structure and homebody call

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from 'react';
 
-import * as S from "./styles";
+import { ButtonComponent } from '@components/index';
+import { HomeBodyComponent } from './subcomponents/HomeBodyComponent';
+
+import * as S from './styles';
+import { cash_logo } from '@assets/index';
 
 export function Home() {
-  return <S.Container></S.Container>;
+  const [openRegistrationModal, setOpenRegistrationModal] = useState<boolean>(false);
+
+  const handleModal = () => {
+    setOpenRegistrationModal(!openRegistrationModal);
+  };
+
+  return (
+    <S.Container>
+      <S.Header>
+        <S.Icon source={cash_logo} />
+        <ButtonComponent
+          onPress={() => handleModal()}
+          text="Nova transação"
+          color="light_green"
+          text_size={12}
+          width="130px"
+          height="35px"
+        />
+      </S.Header>
+
+      <HomeBodyComponent openRegistrationModal={openRegistrationModal} handleModal={handleModal} />
+    </S.Container>
+  );
 }
