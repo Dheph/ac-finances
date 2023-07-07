518a9bb26aae1d958c210d1d168ac8e524332827
Dhepherson Ribeiro <dhephersonribeiro@Dhephersons-MacBook-Air.local>
2023-07-07 12:35:11 -0300

    add: new image files

---
+++ src/assets/index.tsx
@@ -0,0 +1,9 @@
+import green_arrow from './green_arrow.png';
+import red_arrow from './red_arrow.png';
+import cash_icon from './cash_icon.png';
+import cash_logo from './cash-logo.png';
+import ac_logo from './ac-logo.png';
+import ac_logo_nobg from './ac-logo-nobg.png';
+import close_button from './close-button.png';
+
+export { green_arrow, red_arrow, cash_icon, cash_logo, ac_logo, ac_logo_nobg, close_button };
