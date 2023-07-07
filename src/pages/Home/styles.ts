eafa53719611b233be03fab9deca4685a1514f97
Dhepherson Ribeiro <dhephersonribeiro@Dhephersons-MacBook-Air.local>
2023-07-07 12:32:10 -0300

    add: home style container and header

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,19 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.SafeAreaView`
+  height: 100%;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.primary_200};
+  align-items: center;
+`;
+
+export const Header = styled.View`
+  padding-top: 15%;
+  height: 35%;
+  width: 90%;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: ${({ theme }) => theme.colors.primary_200};
+`;
+
+export const Icon = styled.Image``;
